8bf9dcaa3e8f44108d7598180b0afa6751f6ee57
Nikita Vasilikhin <vasilikhinnikita@gmail.com>
2015-05-24 17:54:17 +0300

    Потерялся файл

---
+++ app/src/main/obj/local/x86/objs/AudioProcessor/SuperpoweredAndroidAudioIO.o.d
@@ -0,0 +1,197 @@
+D:/Android/EarsExApp/app/src/main//obj/local/x86/objs/AudioProcessor/SuperpoweredAndroidAudioIO.o: \
+  D:/Android/EarsExApp/app/src/main//jni/SuperpoweredAndroidAudioIO.cpp \
+  D:/Android/EarsExApp/app/src/main//jni/SuperpoweredAndroidAudioIO.h \
+  D:/Android/NDK/platforms/android-9/arch-x86/usr/include\android/log.h \
+  D:/Android/NDK/sources/cxx-stl/stlport/stlport\stdarg.h \
+  D:/Android/NDK/sources/cxx-stl/stlport/stlport\stl/_cprolog.h \
+  D:/Android/NDK/sources/cxx-stl/stlport/stlport\stl/config/features.h \
+  D:/Android/NDK/sources/cxx-stl/stlport/stlport\stl/_stlport_version.h \
+  D:/Android/NDK/sources/cxx-stl/stlport/stlport\stl/config/user_config.h \
+  D:/Android/NDK/sources/cxx-stl/stlport/stlport\stl/config/compat.h \
+  D:/Android/NDK/sources/cxx-stl/stlport/stlport\stl/config/host.h \
+  D:/Android/NDK/sources/cxx-stl/stlport/stlport\stl/config/_system.h \
+  D:/Android/NDK/sources/cxx-stl/stlport/stlport\stl/config/_android.h \
+  D:/Android/NDK/platforms/android-9/arch-x86/usr/include\sys/cdefs.h \
+  D:/Android/NDK/platforms/android-9/arch-x86/usr/include\sys/cdefs_elf.h \
+  D:/Android/NDK/platforms/android-9/arch-x86/usr/include\android/api-level.h \
+  D:/Android/NDK/sources/cxx-stl/stlport/stlport\stl/config/_gcc.h \
+  D:/Android/NDK/sources/cxx-stl/stlport/stlport\stl/config/stl_confix.h \
+  D:/Android/NDK/sources/cxx-stl/stlport/stlport\stl/config/_native_headers.h \
+  D:/Android/NDK/sources/cxx-stl/stlport/stlport\stl/_epilog.h \
+  D:/Android/NDK/sources/cxx-stl/stlport/stlport\stl/_config_compat_post.h \
+  D:/Android/NDK/platforms/android-9/arch-x86/usr/include\SLES/OpenSLES.h \
+  D:/Android/NDK/platforms/android-9/arch-x86/usr/include\SLES/OpenSLES_Platform.h \
+  D:/Android/NDK/platforms/android-9/arch-x86/usr/include\SLES/OpenSLES_Android.h \
+  D:/Android/NDK/sources/cxx-stl/stlport/stlport\pthread.h \
+  D:/Android/NDK/sources/cxx-stl/stlport/stlport\stl/_prolog.h \
+  D:/Android/NDK/platforms/android-9/arch-x86/usr/include\../include/pthread.h \
+  D:/Android/NDK/sources/cxx-stl/stlport/stlport\time.h \
+  D:/Android/NDK/platforms/android-9/arch-x86/usr/include\../include/time.h \
+  D:/Android/NDK/platforms/android-9/arch-x86/usr/include\sys/time.h \
+  D:/Android/NDK/platforms/android-9/arch-x86/usr/include\sys/types.h \
+  D:/Android/NDK/sources/cxx-stl/stlport/stlport\stddef.h \
+  D:/Android/NDK/platforms/android-9/arch-x86/usr/include\stdint.h \
+  D:/Android/NDK/platforms/android-9/arch-x86/usr/include\sys/_types.h \
+  D:/Android/NDK/platforms/android-9/arch-x86/usr/include\machine/_types.h \
+  D:/Android/NDK/platforms/android-9/arch-x86/usr/include\linux/posix_types.h \
+  D:/Android/NDK/platforms/android-9/arch-x86/usr/include\linux/stddef.h \
+  D:/Android/NDK/platforms/android-9/arch-x86/usr/include\linux/compiler.h \
+  D:/Android/NDK/platforms/android-9/arch-x86/usr/include\asm/posix_types.h \
+  D:/Android/NDK/platforms/android-9/arch-x86/usr/include\asm/posix_types_32.h \
+  D:/Android/NDK/platforms/android-9/arch-x86/usr/include\asm/types.h \
+  D:/Android/NDK/platforms/android-9/arch-x86/usr/include\linux/types.h \
+  D:/Android/NDK/platforms/android-9/arch-x86/usr/include\machine/kernel.h \
+  D:/Android/NDK/platforms/android-9/arch-x86/usr/include\sys/sysmacros.h \
+  D:/Android/NDK/platforms/android-9/arch-x86/usr/include\linux/time.h \
+  D:/Android/NDK/platforms/android-9/arch-x86/usr/include\asm/siginfo.h \
+  D:/Android/NDK/platforms/android-9/arch-x86/usr/include\asm-generic/siginfo.h \
+  D:/Android/NDK/sources/cxx-stl/stlport/stlport\signal.h \
+  D:/Android/NDK/platforms/android-9/arch-x86/usr/include\../include/signal.h \
+  D:/Android/NDK/sources/cxx-stl/stlport/stlport\limits.h \
+  D:/Android/NDK/platforms/android-9/arch-x86/usr/include\../include/limits.h \
+  D:/Android/NDK/platforms/android-9/arch-x86/usr/include\sys/limits.h \
+  D:/Android/NDK/platforms/android-9/arch-x86/usr/include\linux/limits.h \
+  D:/Android/NDK/platforms/android-9/arch-x86/usr/include\machine/internal_types.h \
+  D:/Android/NDK/platforms/android-9/arch-x86/usr/include\machine/limits.h \
+  D:/Android/NDK/platforms/android-9/arch-x86/usr/include\sys/syslimits.h \
+  D:/Android/NDK/platforms/android-9/arch-x86/usr/include\asm/page.h \
+  D:/Android/NDK/platforms/android-9/arch-x86/usr/include\asm/page_32.h \
+  D:/Android/NDK/sources/cxx-stl/stlport/stlport\string.h \
+  D:/Android/NDK/platforms/android-9/arch-x86/usr/include\../include/string.h \
+  D:/Android/NDK/platforms/android-9/arch-x86/usr/include\malloc.h \
+  D:/Android/NDK/platforms/android-9/arch-x86/usr/include\asm/signal.h \
+  D:/Android/NDK/platforms/android-9/arch-x86/usr/include\asm-generic/signal.h \
+  D:/Android/NDK/platforms/android-9/arch-x86/usr/include\asm/sigcontext.h \
+  D:/Android/NDK/platforms/android-9/arch-x86/usr/include\sys/ucontext.h \
+  D:/Android/NDK/platforms/android-9/arch-x86/usr/include\sys/user.h \
+  D:/Android/NDK/platforms/android-9/arch-x86/usr/include\sched.h
+
+D:/Android/EarsExApp/app/src/main//jni/SuperpoweredAndroidAudioIO.h:
+
+D:/Android/NDK/platforms/android-9/arch-x86/usr/include\android/log.h:
+
+D:/Android/NDK/sources/cxx-stl/stlport/stlport\stdarg.h:
+
+D:/Android/NDK/sources/cxx-stl/stlport/stlport\stl/_cprolog.h:
+
+D:/Android/NDK/sources/cxx-stl/stlport/stlport\stl/config/features.h:
+
+D:/Android/NDK/sources/cxx-stl/stlport/stlport\stl/_stlport_version.h:
+
+D:/Android/NDK/sources/cxx-stl/stlport/stlport\stl/config/user_config.h:
+
+D:/Android/NDK/sources/cxx-stl/stlport/stlport\stl/config/compat.h:
+
+D:/Android/NDK/sources/cxx-stl/stlport/stlport\stl/config/host.h:
+
+D:/Android/NDK/sources/cxx-stl/stlport/stlport\stl/config/_system.h:
+
+D:/Android/NDK/sources/cxx-stl/stlport/stlport\stl/config/_android.h:
+
+D:/Android/NDK/platforms/android-9/arch-x86/usr/include\sys/cdefs.h:
+
+D:/Android/NDK/platforms/android-9/arch-x86/usr/include\sys/cdefs_elf.h:
+
+D:/Android/NDK/platforms/android-9/arch-x86/usr/include\android/api-level.h:
+
+D:/Android/NDK/sources/cxx-stl/stlport/stlport\stl/config/_gcc.h:
+
+D:/Android/NDK/sources/cxx-stl/stlport/stlport\stl/config/stl_confix.h:
+
+D:/Android/NDK/sources/cxx-stl/stlport/stlport\stl/config/_native_headers.h:
+
+D:/Android/NDK/sources/cxx-stl/stlport/stlport\stl/_epilog.h:
+
+D:/Android/NDK/sources/cxx-stl/stlport/stlport\stl/_config_compat_post.h:
+
+D:/Android/NDK/platforms/android-9/arch-x86/usr/include\SLES/OpenSLES.h:
+
+D:/Android/NDK/platforms/android-9/arch-x86/usr/include\SLES/OpenSLES_Platform.h:
+
+D:/Android/NDK/platforms/android-9/arch-x86/usr/include\SLES/OpenSLES_Android.h:
+
+D:/Android/NDK/sources/cxx-stl/stlport/stlport\pthread.h:
+
+D:/Android/NDK/sources/cxx-stl/stlport/stlport\stl/_prolog.h:
+
+D:/Android/NDK/platforms/android-9/arch-x86/usr/include\../include/pthread.h:
+
+D:/Android/NDK/sources/cxx-stl/stlport/stlport\time.h:
+
+D:/Android/NDK/platforms/android-9/arch-x86/usr/include\../include/time.h:
+
+D:/Android/NDK/platforms/android-9/arch-x86/usr/include\sys/time.h:
+
+D:/Android/NDK/platforms/android-9/arch-x86/usr/include\sys/types.h:
+
+D:/Android/NDK/sources/cxx-stl/stlport/stlport\stddef.h:
+
+D:/Android/NDK/platforms/android-9/arch-x86/usr/include\stdint.h:
+
+D:/Android/NDK/platforms/android-9/arch-x86/usr/include\sys/_types.h:
+
+D:/Android/NDK/platforms/android-9/arch-x86/usr/include\machine/_types.h:
+
+D:/Android/NDK/platforms/android-9/arch-x86/usr/include\linux/posix_types.h:
+
+D:/Android/NDK/platforms/android-9/arch-x86/usr/include\linux/stddef.h:
+
+D:/Android/NDK/platforms/android-9/arch-x86/usr/include\linux/compiler.h:
+
+D:/Android/NDK/platforms/android-9/arch-x86/usr/include\asm/posix_types.h:
+
+D:/Android/NDK/platforms/android-9/arch-x86/usr/include\asm/posix_types_32.h:
+
+D:/Android/NDK/platforms/android-9/arch-x86/usr/include\asm/types.h:
+
+D:/Android/NDK/platforms/android-9/arch-x86/usr/include\linux/types.h:
+
+D:/Android/NDK/platforms/android-9/arch-x86/usr/include\machine/kernel.h:
+
+D:/Android/NDK/platforms/android-9/arch-x86/usr/include\sys/sysmacros.h:
+
+D:/Android/NDK/platforms/android-9/arch-x86/usr/include\linux/time.h:
+
+D:/Android/NDK/platforms/android-9/arch-x86/usr/include\asm/siginfo.h:
+
+D:/Android/NDK/platforms/android-9/arch-x86/usr/include\asm-generic/siginfo.h:
+
+D:/Android/NDK/sources/cxx-stl/stlport/stlport\signal.h:
+
+D:/Android/NDK/platforms/android-9/arch-x86/usr/include\../include/signal.h:
+
+D:/Android/NDK/sources/cxx-stl/stlport/stlport\limits.h:
+
+D:/Android/NDK/platforms/android-9/arch-x86/usr/include\../include/limits.h:
+
+D:/Android/NDK/platforms/android-9/arch-x86/usr/include\sys/limits.h:
+
+D:/Android/NDK/platforms/android-9/arch-x86/usr/include\linux/limits.h:
+
+D:/Android/NDK/platforms/android-9/arch-x86/usr/include\machine/internal_types.h:
+
+D:/Android/NDK/platforms/android-9/arch-x86/usr/include\machine/limits.h:
+
+D:/Android/NDK/platforms/android-9/arch-x86/usr/include\sys/syslimits.h:
+
+D:/Android/NDK/platforms/android-9/arch-x86/usr/include\asm/page.h:
+
+D:/Android/NDK/platforms/android-9/arch-x86/usr/include\asm/page_32.h:
+
+D:/Android/NDK/sources/cxx-stl/stlport/stlport\string.h:
+
+D:/Android/NDK/platforms/android-9/arch-x86/usr/include\../include/string.h:
+
+D:/Android/NDK/platforms/android-9/arch-x86/usr/include\malloc.h:
+
+D:/Android/NDK/platforms/android-9/arch-x86/usr/include\asm/signal.h:
+
+D:/Android/NDK/platforms/android-9/arch-x86/usr/include\asm-generic/signal.h:
+
+D:/Android/NDK/platforms/android-9/arch-x86/usr/include\asm/sigcontext.h:
+
+D:/Android/NDK/platforms/android-9/arch-x86/usr/include\sys/ucontext.h:
+
+D:/Android/NDK/platforms/android-9/arch-x86/usr/include\sys/user.h:
+
+D:/Android/NDK/platforms/android-9/arch-x86/usr/include\sched.h:
